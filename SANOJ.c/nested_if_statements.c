9609eebc8a366499bf8bdcc91b50adabeec5cceb
Sanoj K <ssanojmanoj2006@gmail.com>
2026-02-01 19:27:03 +0530

    nested_if_statements

--- SANOJ.c/nested_if_statements.c
+++ SANOJ.c/nested_if_statements.c
@@ -1,23 +1,75 @@
+//#include <stdio.h>
+//#include <stdbool.h>
+
+//int main()
+//{
+    //float price = 10.00;
+    //bool isStudent = false;
+    //bool isSenior = false;
+
+    //if(isStudent)
+    //{
+        //printf("you get a student discount 0f 20%\n");
+        //price *=0.9;
+    //}
+    //if(isSenior)
+    //{
+        //printf("you get a senior discount 0f 20%\n");
+        //price *=0.8;
+    //}
+    //printf("the price of a ticket is: $%.2f\n",price);
+
+    //return 0;
+//}
+
+
+
+//[EE RETHILLAN NAMAL CHEYTHIRUNNATH BUT EEPOLL NAMMEL INNE CHEYAN POKUNATH VERE RITHI]
+
 #include <stdio.h>
 #include <stdbool.h>
 
 int main()
-{
-    float price = 10.00;
-    bool isStudent = false;
-    bool isSenior = false;
-
-    if(isStudent)
     {
-        printf("you get a student discount 0f 10%\n");
-        price *=0.9;
-    }
-    if(isSenior)
-    {
-        printf("you get a senior discount 0f 20%\n");
-        price *=0.8;
-    }
-    printf("the price of a ticket is: $%.2f\n",price);
+        float price = 10.00;
+        bool isStudent = false;
+        bool isSenior = true;
 
-    return 0;
-}
+        if(isStudent)
+        {
+            if(isSenior)
+            {
+                printf("you get a student discount 0f 10%\n");
+                printf("you get a senior discount 0f 20%\n");
+                price = 0.8; 
+                
+            }
+            else
+            {
+                printf("you get a senior discount 0f 10%\n");
+                price = 0.9;
+            }
+        }
+        else
+        {
+            if(isSenior)
+            {
+                printf("you get a senior discount 0f 20%\n");
+                price = 0.8;
+            }
+        }
+
+        printf("the price of a ticket is: $%.2f\n",price);
+
+        return 0;
+    }
+
+
+
+
+
+
+
+
+            
+
