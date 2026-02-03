df802035a01714463f3bdff3d273d9131ac444bb
Sanoj K <ssanojmanoj2006@gmail.com>
2026-02-03 21:38:54 +0530

    calculator_program

---
+++ SANOJ.c/calculator_program.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+int main()
+{
+
+    // CALCULATOR PROGRAM
+
+char operator = '\0';
+double num1 = 0.0;
+double num2 = 0.0;
+double result = 0.0;
+
+printf("enter the first number: ");
+scanf("%lf", &num1);
+
+printf("enter the operator + - * / : ");
+scanf(" %c", &operator);  // Notice the space before %c
+
+
+printf("enter the second number: ");
+scanf("%lf", &num2);
+
+
+switch(operator)
+{
+    case '+':
+        result = num1+num2;
+        break;
+
+    case '-':
+        result = num1-num2;
+        break;  
+
+    case '*':
+        result = num1*num2;
+        break;
+        
+    case '/':
+        if(num2==0)
+        {
+            printf("you can't divide by zero!\n");
+        }
+        else
+        {
+            result = num1/num2;
+        }
+        break;
+
+        
+
+
+
+    default:
+        printf("invalid statement");
+        break;
+        
+}
+
+printf("result: %.4lf",result);
+
+return 0;
+
+}
